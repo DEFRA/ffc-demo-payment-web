5ef5142dcbd27141962118d26633047698ac9e21
Mark Harrop <mark.harrop@defra.gov.uk>
2020-03-25 13:35:32 +0000

    add simple Okta auth at top level of site

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,6 +2,10 @@ module.exports = {
   method: 'GET',
   path: '/',
   options: {
+    auth: {
+      mode: 'required',
+      strategy: 'okta'
+    },
     handler: (request, h) => {
       return h.view('home')
     }
