bd2ca722ac511feaa1b28beae59df2c2d41e493b
Mark Harrop <mark.harrop@defra.gov.uk>
2020-05-05 11:20:09 +0100

    remove comments and unused parameters

--- app/plugins/b2c-custom-provider.js
+++ app/plugins/b2c-custom-provider.js
@@ -1,14 +1,13 @@
 const jwtDecode = require('jwt-decode')
 
 function b2cCustomProvider (options) {
-  console.log(`openid+${options.clientId}`)
   return {
     protocol: 'oauth2',
     useParamsAuth: true,
     auth: `${options.uri}/oauth2/v2.0/authorize`,
     token: `${options.uri}/oauth2/v2.0/token`,
     scope: [`openid ${options.clientId}`],
-    profile: async function (credentials, params, get) {
+    profile: async function (credentials) {
       const { givenName, surname } = jwtDecode(credentials.token)
       credentials.profile = {
         firstName: givenName,
@@ -20,12 +19,3 @@ function b2cCustomProvider (options) {
 }
 
 module.exports = b2cCustomProvider
-/*
-        id: profile.sub,
-        username: profile.email,
-        displayName: profile.nickname,
-        firstName: profile.given_name,
-        lastName: profile.family_name,
-        email: profile.email,
-        raw: profile
-        */
