7abb555ced8e637346bf88295791007c1f3e98d9
Phil Benson <phil.benson@defra.gov.uk>
2020-05-06 16:55:52 +0100

    Added step to publish Pact to Pact Broker

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,11 @@
 @Library('defra-library@4') _
 
-buildNodeJs environment: 'dev'
+buildNodeJs environment: 'dev',
+  testClosure: () => {
+    withCredentials([
+      string(credentialsId: 'pact-broker-url', variable: 'pactBrokerURL')
+    ]) {
+      // publish pact to broker
+      sh "node $WORKSPACE/test/contract/publish-contract.js --pactBroker=$pactBrokerURL"
+    }
+  }
--- test/contract/publish-contract.js
+++ test/contract/publish-contract.js
@@ -2,12 +2,27 @@ const { Publisher } = require('@pact-foundation/pact')
 const path = require('path')
 const packageJson = require('../../package.json')
 
+const getPactBrokerURL = () => {
+  const pactBrokerArg = process.argv.find(arg => arg.startsWith('--pactBroker'))
+
+  if (pactBrokerArg) {
+    return pactBrokerArg.split('=')[1]
+  } else if (process.env.PACT_BROKER) {
+    return process.env.PACT_BROKER
+  }
+}
+
+const pactBroker = getPactBrokerURL()
+
 const opts = {
   pactFilesOrDirs: [path.resolve(process.cwd(), 'test-output')],
-  pactBroker: process.env.PACT_BROKER || 'http://localhost:9292',
+  pactBroker,
   consumerVersion: packageJson.version,
   pactBrokerUsername: 'pact',
   pactBrokerPassword: 'pact'
 }
 
-new Publisher(opts).publishPacts()
+if (pactBroker) {
+  console.log(`Publishing Pacts to ${pactBroker}`)
+  new Publisher(opts).publishPacts()
+}
