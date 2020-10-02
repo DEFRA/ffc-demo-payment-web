2babb999a3cfc8cea8e035d9806f2aaad71d7dce
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2020-10-02 16:01:22 +0100

    feat: add code to log if app insights running or not (#40)

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -2,11 +2,13 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    console.log('Registering Application Insights')
     appInsights.setup().start()
+    console.log('App Insights Running')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
+  } else {
+    console.log('App Insights Not Running!')
   }
 }
 
